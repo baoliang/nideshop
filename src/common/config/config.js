4979b53cd99cd74ca0e8634ce5ebe0672108de39
zhangbaoliang <zhangbaoliang.star@gmail.com>
2018-04-13 07:31:43 +0800

    wechat config

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid: '', // 小程序 appid
-    secret: '', // 小程序密钥
+    appid: 'wx0a10ba00b713c337', // 小程序 appid
+    secret: 'wx0a10ba00b713c337', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
